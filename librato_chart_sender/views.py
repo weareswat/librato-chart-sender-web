f31a05a5a44c23004dd70a374e3e7e8d15f66b0a
DJGuu <goncalo.correia@rupeal.com>
2016-06-20 11:44:10 +0100

    connects to database and sends the data

--- librato_chart_sender/views.py
+++ librato_chart_sender/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .forms import NewConfigForm
 from django.shortcuts import redirect
+from .models import Configuration
 
 def index(request):
     template = loader.get_template('index.html')
@@ -12,6 +13,14 @@ def config_new(request):
     if request.method == "POST":
         form = NewConfigForm(request.POST)
         if form.is_valid():
+            new_config = Configuration(librato_user = form.get_form_values()['email_username'],
+                                       librato_api_key = form.get_form_values()['api_key'],
+                                       recipients_emails = ','.join(form.get_form_values()['recipients']),
+                                       chart_ids = ','.join(form.get_form_values()['chart_ids']),
+                                       interval = 1,
+                                       duration = 2,
+                                       template = 'something to change')
+            new_config.save()
             return redirect('index')
         else:
             error_list = {'error_list': form.get_errors()}
