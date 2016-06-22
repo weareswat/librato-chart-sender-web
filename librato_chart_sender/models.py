085dbe10dd150c821f2ad678c4dc9d895663a9d1
DJGuu <goncalo.correia@rupeal.com>
2016-06-22 17:36:16 +0100

    modified model

--- librato_chart_sender/models.py
+++ librato_chart_sender/models.py
@@ -10,12 +10,11 @@ class Configuration(models.Model):
     librato_api_key = models.CharField(max_length=100)
     recipients_emails = models.TextField()
     chart_ids = models.CommaSeparatedIntegerField(max_length=250)
-    interval = models.IntegerField()
+    interval = models.CharField(max_length=250)
     duration = models.IntegerField()
-    template = models.CharField(max_length=100)
 
     def __str__(self):
-        return " | ".join([self.librato_user, self.librato_api_key, self.recipients_emails, self.chart_ids, str(self.interval), str(self.duration), self.template])
+        return " | ".join([self.librato_user, self.librato_api_key, self.recipients_emails, self.chart_ids, str(self.interval), str(self.duration)])
 
     def separate_chart_ids(self):
         return ' , '.join(self.chart_ids.split(','))
