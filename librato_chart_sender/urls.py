21276fe45b4de06270b3207e346af96db96f1541
pawel-krysiak <pawel.krysiak@rupeal.com>
2016-06-27 17:14:46 +0100

    added send now option

--- librato_chart_sender/urls.py
+++ librato_chart_sender/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^config/new/$', views.config_new, name='config-new'),
     url(r'^config/edit/(?P<config_id>[0-9]+)/$', views.config_edit, name='config-edit'),
     url(r'^config/delete/(?P<config_id>[0-9]+)/$', views.config_delete, name='config-delete'),
+    url(r'^config/send_now/(?P<config_id>[0-9]+)/$', views.send_now, name='send-now'),
 ]
--- librato_chart_sender/views.py
+++ librato_chart_sender/views.py
@@ -3,6 +3,7 @@ from .forms import NewConfigForm
 from django.shortcuts import redirect
 from .models import Configuration
 from db.config_db import ConfigDB
+from chart_sender import LibratoChartSender
 
 
 def index(request):
@@ -59,4 +60,11 @@ def config_edit(request, config_id):
 
 def config_delete(request, config_id):
     ConfigDB.delete_configuration(config_id)
+    return redirect('index')
+
+def send_now(request, config_id):
+    chart_sender = LibratoChartSender([3419, 3420, 3421], ['pawel.krysiak@rupeal.com'], {
+        'librato_api_key': 'b4bf0341c8cdd3b429826a18d1a07582895fa12c7fb97eb8f2c6bdb015004b86',
+        'mailgun_api_key': 'key-a05af654983f6c57ec99904a3b84c7b3'})
+    chart_sender.run()
     return redirect('index')
