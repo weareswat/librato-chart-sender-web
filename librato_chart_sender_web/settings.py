f9a2bf9e00b6615232d5f3aac415b80346df74c4
pawel-krysiak <pawel.krysiak@rupeal.com>
2016-06-28 17:21:31 +0100

    modified logging format

--- librato_chart_sender_web/settings.py
+++ librato_chart_sender_web/settings.py
@@ -137,25 +137,10 @@ STATICFILES_DIRS = [
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
     'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
         }
     },
     'loggers': {
