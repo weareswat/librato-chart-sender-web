fdd5aed614660d8b4e426f435da09e0e9523e33f
pawel-krysiak <pawel.krysiak@rupeal.com>
2016-06-29 15:29:19 +0100

    Added authentication mechanism

--- librato_chart_sender/urls.py
+++ librato_chart_sender/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^config/edit/(?P<config_id>[0-9]+)/$', views.config_edit, name='config-edit'),
     url(r'^config/delete/(?P<config_id>[0-9]+)/$', views.config_delete, name='config-delete'),
     url(r'^config/send_now/(?P<config_id>[0-9]+)/$', views.send_now, name='send-now'),
+    url(r'^login/$', views.login_user),
+    url(r'^logout/$', views.logout_user),
 ]
